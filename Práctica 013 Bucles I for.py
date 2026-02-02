2947a035d93670394b3078bba691bc221c94b938
Leisberth Cedeño <>
2026-02-02 11:45:22 -0500

    Commit práctica condicionales en Python

---
+++ Práctica 013 Bucles I for.py
@@ -0,0 +1,23 @@
+# BUCLES
+
+# Intrucción
+
+# Declaración del bucle
+    # cuerpo del  bucle
+
+# Intrucción
+# Intrucción
+
+
+# for variable in elemento a recorrer: 
+    # cuerpo del  bucle
+
+for i in[2, 6, 6]: # in elemento a recorrer
+    print("Hola") 
+
+
+for i in range(4): # range(4) genera una secuencia de números del 0 al 3
+    print("Hola")
+
+for i in ["Enero", "Febrero", "Marzo", "Abril"]:
+    print(i) # imprime el valor de la variable i en cada iteración
